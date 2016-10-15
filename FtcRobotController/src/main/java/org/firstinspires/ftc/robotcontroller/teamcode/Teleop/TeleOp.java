12aab62c8e83b615c3e4f38594bc078cf8923b01
SamPhilip <samuel.philip@gmail.com>
2016-10-15 17:13:37 -0400

    V2.0.0.1
    Tele-Op Mode Added

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/teamcode/Teleop/TeleOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/teamcode/Teleop/TeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.robotcontroller.teamcode.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -13,9 +14,21 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class TeleOp extends LinearOpMode {
     DcMotor L;
-
+    DcMotor R;
+    private void InitializeRobot() {
+        L = hardwareMap.dcMotor.get("L");
+        L.setDirection(DcMotorSimple.Direction.REVERSE);
+        R = hardwareMap.dcMotor.get("R");
+    }
     @Override
     public void runOpMode() throws InterruptedException {
-
+        InitializeRobot();
+        waitForStart();
+        while (opModeIsActive()) {
+            L.setMaxSpeed(2048);
+            R.setMaxSpeed(2048);
+            L.setPower(gamepad1.left_stick_y);
+            R.setPower(gamepad1.right_stick_y);
+        }
     }
 }
